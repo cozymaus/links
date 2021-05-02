1cf4856f54944f4e6e1ec6cf60d471c175f0b63c
Liz Berberena <31869677+lizberberena@users.noreply.github.com>
2021-05-02 14:10:00 -0400

    Update mode-switcher.js

--- mode-switcher.js
+++ mode-switcher.js
@@ -1,62 +1,53 @@
-// this checks whether system dark mode is set 
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
-// this checks for session storage telling to override
-// the system preferences 
 let theme = sessionStorage.getItem('theme');
-  
+
 if (systemInitiatedDark.matches) {
-	document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-sun"></i>";
+	document.getElementById("theme-toggle").innerHTML = "Light Mode";
 } else {
-	document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-moon"></i>";
+	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
-  
+
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');		
-   	document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-sun"></i>";
-   	// this clears the session storage 
+   	document.getElementById("theme-toggle").innerHTML = "Light Mode";
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-moon"></i>";
+    document.getElementById("theme-toggle").innerHTML = "Dark Mode";
     sessionStorage.setItem('theme', '');
   }
 }
 systemInitiatedDark.addListener(prefersColorTest);
-  
-  function modeSwitcher() {
-// it’s important to check for overrides again 
+
+
+function modeSwitcher() {
 	let theme = sessionStorage.getItem('theme');
-	// checks if reader selected dark mode 
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-moon"></i>";
-		// checks if reader selected light mode 
+		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-sun"></i>";
-		// checks if system set dark mode 
+		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {	
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-moon"></i>";
-		// the only option left is system set light mode
+		document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-sun"></i>";
+		document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }
-  
-  
-  if (theme === "dark") {
+
+if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-sun"></i>";
+	document.getElementById("theme-toggle").innerHTML = "Light Mode";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("toggle-theme").innerHTML = "<i class="fas fa-moon"></i>";
+	document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 }
